e06affb45f681ae4c7a168b27204307dc2f09739
Sushant Sharma <sushant.x01@gmail.com>
2022-07-02 16:46:13 +0530

    Socket.io setup complete

--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000/",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(5000, () => {
   console.log("Server is runing on port 5000.");
